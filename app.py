2cd54e06c8eeb2bf5e940e982397796186af5679
Madhur Gupta <114422076+MadhurGupta0@users.noreply.github.com>
2024-02-23 13:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,12 +33,12 @@ with st.sidebar:
             st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
     st.subheader('Models')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B','Llama2-70b'], key='selected_model')
     if selected_model == 'Llama2-7B':
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
     elif selected_model == 'Llama2-13B':
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    elif selected_model=="Llama2-70b":
+    elif selected_model=='Llama2-70b':
         llm="meta/codellama-70b-instruct:a279116fe47a0f65701a8817188601e2fe8f4b9e04a518789655ea7b995851bf"
     #clear CHAT history button on slider
     def clear_chat_history():
