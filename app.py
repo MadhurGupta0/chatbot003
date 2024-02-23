611ef327df6d1298d7bf881f5492d1aa321e4634
Madhur Gupta <114422076+MadhurGupta0@users.noreply.github.com>
2024-02-23 12:54:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,11 +96,6 @@ else:
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-    def clear_chat_history():
-        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-    prompt = st.chat_input("Copy bot")
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
